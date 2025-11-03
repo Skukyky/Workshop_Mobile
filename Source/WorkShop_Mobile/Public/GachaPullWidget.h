14a02dbc2a03bbb3ce7bac19d414f5d72997d25d
Cookiecrafty <thomas.boinet75@gmail.com>
2025-11-03 17:03:50 +0100

    gacha UI Update

--- Source/WorkShop_Mobile/Public/GachaPullWidget.h
+++ Source/WorkShop_Mobile/Public/GachaPullWidget.h
@@ -6,6 +6,8 @@
 #include "Blueprint/UserWidget.h"
 #include "GachaPullWidget.generated.h"
 
+class UButton;
+class UTextBlock;
 /**
  * 
  */
@@ -13,5 +15,25 @@ UCLASS()
 class WORKSHOP_MOBILE_API UGachaPullWidget : public UUserWidget
 {
 	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* BTN_Pull;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* TypeText;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* RarityText;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* NameText;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* StatYoutubeText;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* StatTikTokText;
 	
 };
--- Source/WorkShop_Mobile/WorkShop_Mobile.Build.cs
+++ Source/WorkShop_Mobile/WorkShop_Mobile.Build.cs
@@ -8,7 +8,7 @@ public class WorkShop_Mobile : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AIModule", "NavigationSystem" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AIModule", "NavigationSystem","UMG" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
