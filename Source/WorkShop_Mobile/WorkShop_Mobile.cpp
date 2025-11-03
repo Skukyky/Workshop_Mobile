d6b0cc2724be7c1cf2ceaae2106490796a324603
Skukyky <maxime.arnau@outlook.fr>
2025-11-03 13:31:02 +0100

    Initial commit

---
+++ Source/WorkShop_Mobile/WorkShop_Mobile.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "WorkShop_Mobile.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, WorkShop_Mobile, "WorkShop_Mobile" );
